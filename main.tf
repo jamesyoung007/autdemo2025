5fcbb452fdbadaa9dd2a8bd5f6bb866e41d38d19
jamesyoung007 <jamesyoung007@gmail.com>
2025-05-10 20:15:22 +1200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "my-rg"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "mystorageacct1234" # must be globally unique
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "my-function-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_linux_function_app" "function" {
+  name                       = "my-function-app-1234"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  service_plan_id            = azurerm_app_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  site_config {
+    application_stack {
+      node_version = "~18"
+    }
+  }
+  os_type = "linux"
+}
