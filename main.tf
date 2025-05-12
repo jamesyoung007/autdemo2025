6ba5ab51c0e64fb0a54711082a9144c07bc95d70
shaoniaogong <shaoniaogong@outlook.com>
2025-05-12 19:11:52 +1200

    fixing errors

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-
-
-# ---- Content from main.tf ----
-
-provider "azurerm" {
-  features {}
-  subscription_id = "57480482-27fc-46a6-8643-ee45484365ec"
-  resource_provider_registrations = "none"
-}
-
 terraform {
   backend "azurerm" {
     resource_group_name   = "tfstaterg"
@@ -17,63 +7,57 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+  subscription_id = "57480482-27fc-46a6-8643-ee45484365ec"
+  resource_provider_registrations = "none"
+}
+
+variable "location" {
+  type    = string
+  default = "Australia East"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "AUT-2025-demo"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-module "storage" {
-  source              = "./modules"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+resource "azurerm_storage_account" "storage" {
+  name                     = "autdemostorage1234"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "autdemo-law"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
+resource "azurerm_service_plan" "plan" {
+  name                = "autdemo-function-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
-resource "azurerm_monitor_diagnostic_setting" "sa_diagnostics" {
-  name                       = "diag-sa"
-  target_resource_id         = module.storage.storage_account_id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+resource "azurerm_linux_function_app" "function" {
+  name                       = "autdemo-functionapp1234"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  service_plan_id            = azurerm_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
 
-  log {
-    category = "StorageRead"
-    enabled  = true
-
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
-  }
-
-  log {
-    category = "StorageWrite"
-    enabled  = true
-
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
-  }
-
-  metric {
-    category = "AllMetrics"
-    enabled  = true
-
-    retention_policy {
-      enabled = false
-      days    = 0
+  site_config {
+    application_stack {
+      node_version = "18"
     }
   }
 }
-
-
-# ---- Content from monitoring.tf ----
 
 resource "azurerm_log_analytics_workspace" "law" {
   name                = "autdemo-law"
@@ -85,7 +69,7 @@ resource "azurerm_log_analytics_workspace" "law" {
 
 resource "azurerm_monitor_diagnostic_setting" "storage_diagnostics" {
   name                       = "diag-storage"
-  target_resource_id         = module.storage.storage_account_id
+  target_resource_id         = azurerm_storage_account.storage.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
 
   log {
@@ -153,40 +137,4 @@ resource "azurerm_monitor_diagnostic_setting" "app_service_plan_diagnostics" {
       days    = 0
     }
   }
-}
-
-
-# ---- Content from variables.tf ----
-
-variable "location" {
-  type    = string
-  default = "Australia East"
-}
-
-variable "resource_group_name" {
-  type    = string
-  default = "AUT-2025-demo"
-}
-
-
-# ---- Inline module content from modules/st.tf ----
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "autdemostorage1234"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
-}
-
-output "storage_account_key" {
-  value = azurerm_storage_account.storage.primary_access_key
-}
-
-output "storage_account_id" {
-  value = azurerm_storage_account.storage.id
 }
